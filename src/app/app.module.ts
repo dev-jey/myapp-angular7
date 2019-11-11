05c95be6abd60cbb06e4b0aca47debe9b4e39c6f
jey <jey@jeys-MacBook-Pro.local>
2019-11-11 15:51:35 +0300

    Add a service to get posts from typicode json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -18,7 +18,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  h1Style: boolean = true;
+  btnContent: string = "Dark Theme";
+  todoData: any = []
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getData().subscribe(
+      data => {
+        console.log('data', data)
+        this.todoData = data;
+      },
+      error => {
+        this.todoData = []
+      })
+  }
+
+
+  firstClick = () => {
+    this.h1Style = !this.h1Style;
+    if (this.btnContent == "Light Theme") {
+      this.btnContent = "Dark Theme";
+    } else {
+      this.btnContent = "Light Theme";
+    }
   }
 
 }
